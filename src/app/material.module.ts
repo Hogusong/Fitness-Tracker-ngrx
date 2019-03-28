0fc259cf535fc4c569c0a4db4b3cd733f1646d25
“Young <hogusong1@gmail.com>
2019-03-28 00:42:22 -0400

    import 'Created 'Sidebar' component.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
-import { MatToolbarModule, MatIconModule, MatButtonModule } from '@angular/material';
+import { MatToolbarModule, MatIconModule, MatButtonModule, MatListModule } from '@angular/material';
 
 @NgModule({
   exports: [
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatListModule
   ]
 })
 export class MaterialModule {}
--- src/app/menu/sidebar/sidebar.component.ts
+++ src/app/menu/sidebar/sidebar.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  authStatus = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  login() {
+    this.authStatus = true;
+  }
+
+  logout() {
+    this.authStatus = false;
+  }
 }
